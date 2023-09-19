d5d9918dddc5518fc4d1786122501afe5f04ea45
anrajin <an.rajin@gmail.com>
2023-09-19 22:28:14 +0600

    [new] - initialized tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      transitionProperty: {
+        height: 'max-height'
+      }
+    },
+  },
+  plugins: [],
+}
