0611e556e44605b6833b20e862667b9eae1ec60f
anrajin <an.rajin@gmail.com>
2023-12-06 22:52:15 +0600

    duplicate components removed

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,10 +1,10 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
-import {PublicRoutingModule} from './public-routing.module';
-import {PublicComponent} from './public.component';
-import {HomeComponent} from './home/home.component';
-import {AuthModule} from "./auth/auth.module";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PublicRoutingModule } from './public-routing.module';
+import { PublicComponent } from './public.component';
+import { HomeComponent } from './home/home.component';
+import { AuthModule } from "./auth/auth.module";
 
 
 @NgModule({
