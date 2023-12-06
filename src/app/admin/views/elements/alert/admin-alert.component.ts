ae15758bb5241f05dc76a53dcb22ea53f9017c92
anrajin <an.rajin@gmail.com>
2023-12-06 22:46:59 +0600

    reference correction

--- src/app/admin/views/elements/alert/admin-alert.component.ts
+++ src/app/admin/views/elements/alert/admin-alert.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { pageTransition } from 'src/app/shared/animations';
 import { AlertComponent } from 'src/app/shared/components/alert/alert.component';
 import { AlertType } from 'src/app/shared/components/alert/alert.type';
+import { pageTransition } from 'src/app/shared/utils/animations';
 
 @Component({
   selector: 'admin-alert',
