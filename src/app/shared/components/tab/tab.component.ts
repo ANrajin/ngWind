5bef01e8dfd092a073003850b3a85bce511ba302
anrajin <an.rajin@gmail.com>
2023-12-17 15:20:28 +0600

    [clear] - code clean up

--- src/app/shared/components/tab/tab.component.ts
+++ src/app/shared/components/tab/tab.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {TabContents} from "./tab-contents.type";
 
