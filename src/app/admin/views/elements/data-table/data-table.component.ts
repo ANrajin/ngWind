38ad12bb7e1ca5ddd2d9c140f815d88a00f68945
ferdousr3 <ferdousr1992@gmail.com>
2023-12-19 17:26:12 +0600

    add new search field

--- src/app/admin/views/elements/data-table/data-table.component.ts
+++ src/app/admin/views/elements/data-table/data-table.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { IProduct, TableData } from './table.data';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-data-table',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './data-table.component.html',
-  styleUrl: './data-table.component.css'
+  styleUrl: './data-table.component.css',
 })
 export class DataTableComponent {
-
+  public products: IProduct[] = TableData.products;
+  public pages: number[] = TableData.pageNumber;
 }
--- src/app/admin/views/elements/data-table/table.data.ts
+++ src/app/admin/views/elements/data-table/table.data.ts
@@ -15,5 +15,70 @@ export class TableData {
       category: 'Laptop',
       price: 29999,
     },
+    {
+      id: 2,
+      name: 'Microsoft Surface Pro',
+      color: 'White',
+      category: 'Laptop PC',
+      price: 1999,
+    },
+    {
+      id: 3,
+      name: 'Magic Mouse 2',
+      color: 'Black',
+      category: 'Accessories',
+      price: 99,
+    },
+    {
+      id: 4,
+      name: 'Apple Watch',
+      color: 'Black',
+      category: 'Watches',
+      price: 199,
+    },
+    {
+      id: 5,
+      name: 'Apple iMac',
+      color: 'Silver',
+      category: 'PC',
+      price: 199,
+    },
+    {
+      id: 6,
+      name: 'Apple AirPods',
+      color: 'White',
+      category: 'Accessories',
+      price: 399,
+    },
+    {
+      id: 7,
+      name: 'iPad Pro',
+      color: 'Gold',
+      category: 'Tablet',
+      price: 699,
+    },
+    {
+      id: 8,
+      name: 'Magic Keyboard',
+      color: 'Black',
+      category: 'Accessories',
+      price: 99,
+    },
+    {
+      id: 9,
+      name: 'Smart Folio iPad Air',
+      color: 'Blue',
+      category: 'Accessories',
+      price: 79,
+    },
+    {
+      id: 10,
+      name: 'AirTag',
+      color: 'Silver',
+      category: 'Accessories',
+      price: 29,
+    },
   ];
+
+  public static readonly pageNumber: number[] = [1, 2, 3, 4, 5];
 }
