cbbd85df4d606f7fbfedfcf42b5fe82efbfed48f
Mihadul Islam <mehad65@gmail.com>
2024-01-08 04:19:38 +0600

    pagination component created

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,6 +1,7 @@
 import { NgClass, NgIf } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'pagination',
@@ -11,20 +12,96 @@ import { FormsModule } from '@angular/forms';
 })
 export class PaginationComponent {
    @Input() currentPage:number = 1;
-   @Input() limit: number = 5;
-   @Input() total: number = 100;
-   @Output() changePage = new EventEmitter<number>();
+   @Input() pageSize: number = 10;
+   @Input() total: number =55;
 
-   pages: number [] = [];
+   @Output()
+   changePage = new EventEmitter<number>();
+
+   firstPage:number = 1;
+   pageDifference: number = 5;
+   lastPage: number = this.pageDifference;
+
+
+   pages: Page [] = [];
 
    ngOnInit(): void{
-    const pagesCount = Math.ceil(this.total / this.limit);
-    this.pages = this.range(1, pagesCount);
+      this.preparePages();
+   }
+
+   getPageCount():number{
+    return Math.ceil(this.total / this.pageSize);
+   }
+
+   setCurrentPage(page:Page){
+     this.currentPage = page.pageNumber;
+     this.preparePages();
+   }
+
+   goToPrevPage(){
+    if(this.currentPage == this.firstPage){
+      if(this.currentPage!=1){
+        this.firstPage = this.currentPage - this.pageDifference;
+        this.lastPage = this.firstPage + this.pageDifference-1;
+        this.currentPage = this.currentPage - 1;
+        this.preparePages();
+      }
+      
+    }else{
+      this.currentPage = this.currentPage - 1;
+      this.preparePages();
+    }
+   }
+
+   goToNextPage(){
+    if(this.currentPage == this.lastPage){
+      if(this.currentPage != this.getPageCount()){
+        this.firstPage = this.currentPage+1;
+        this.lastPage = this.generateLastPage();
+        this.currentPage = this.currentPage + 1;
+        this.preparePages();
+      }
+    }else{
+      this.currentPage = this.currentPage + 1;
+      this.preparePages();
+    }
+   }
+
+   generateLastPage(){
+    if(this.getPageCount()<this.pageDifference){
+      this.lastPage = this.getPageCount();
+    }else if(this.firstPage ==((Math.floor(this.getPageCount() / this.pageDifference))*this.pageDifference)+1){
+      this.lastPage = this.getPageCount();
+    }else{
+      this.lastPage= this.firstPage + this.pageDifference-1;
+    }
     
+    return this.lastPage;
+   }
+
+   preparePages(){
+    this.changePage.emit(this.currentPage);
+    const ranges: number[] = this.range(this.firstPage, (this.generateLastPage()-this.firstPage)+1);
+    this.pages = [];
+    this.pages = ranges.map(x =>{
+      return {
+        isFirst: x===this.firstPage,
+        isLast : x===this.lastPage,
+        isActive:x===this.currentPage,
+        pageNumber: x
+      }
+    });
    }
 
    range(start:number, end:number): number[]{
     return [...Array(end).keys()].map((el)  => el + start);
    }
 
+}
+
+export interface Page{
+  isFirst: boolean;
+  isLast: boolean;
+  isActive:boolean;
+  pageNumber: number;
 }
