ae85498bd4f9d2eaa09874513cf3feb7265da2cb
Mihadul <mehad65@gmail.com>
2024-01-08 11:55:11 +0600

    removed unused code from here

--- src/app/admin/views/elements/pagination/pagination.component.ts
+++ src/app/admin/views/elements/pagination/pagination.component.ts
@@ -12,7 +12,7 @@ import { TableData } from '../data-table/table.data';
 })
 export class AdminPaginationComponent {
   public pages: number[] = TableData.pageNumber;
-  getPageIndex(index:number){
-    console.log(index);
+  getPageIndex(index: number) {
+    //your code goes here
   }
 }
