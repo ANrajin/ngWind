d3dc112565129de24876acbf886bf783c1f0a270
anrajin <an.rajin@gmail.com>
2024-01-31 10:18:37 +0600

    [new] - new color palette added

--- src/app/admin/layouts/header/header.component.ts
+++ src/app/admin/layouts/header/header.component.ts
@@ -8,10 +8,10 @@ import { Images } from 'src/assets/data/images';
 })
 export class HeaderComponent {
   public userOne: string = Images.users.userOne;
-  
+
   isOpen: boolean = false;
 
-  constructor(private element: ElementRef, private renderer: Renderer2) {}
+  constructor(private element: ElementRef, private renderer: Renderer2) { }
 
   onClickProfile = () => {
     const profileDropdownList = this.element.nativeElement.querySelector(
@@ -19,4 +19,27 @@ export class HeaderComponent {
     );
     this.renderer.setAttribute(profileDropdownList, 'aria-expanded', 'true');
   };
+
+  toggleTheme = () => {
+    // If set via local storage previously
+    if (localStorage.getItem("color-theme")) {
+      if (localStorage.getItem("color-theme") === "light") {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      }
+
+      // If NOT set via local storage previously
+    } else {
+      if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      } else {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      }
+    }
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
+  darkMode: 'class',
   theme: {
     extend: {
+      colors: {
+        "primary": "rgba(var(--primary))",
+        "primary-light": "rgba(var(--primary-light))",
+        "theme": "rgba(var(--theme))",
+        "title": "rgba(var(--title))",
+        "card": "rgba(var(--card))",
+      },
       container: {
         center: true,
         padding: "1.5rem",
